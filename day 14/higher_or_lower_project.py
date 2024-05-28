cae3ae3ca98ac849ab3df09a57eb1ca7debb2235
Ur Koren <ork14790@gmail.com>
2024-05-28 22:18:45 +0300

    Update higher_or_lower_project.py

--- day 14/higher_or_lower_project.py
+++ day 14/higher_or_lower_project.py
@@ -1,57 +1,47 @@
-import random
-
-import game_data
-
 from game_data import data
-
 from random import randint
 
 
-def compare_followers(followers_b, followers_a, compare_b):
-    global user_score
+def compare_followers(followers_b, followers_a, person_b):
     global end_game
+
     print(
-        f"Against B: {compare_b["name"]}, a {compare_b['description']}, from {compare_b['country']}. followers number: {followers_b}"
+        f"A: {person_a["name"]}, a {person_a['description']}, from {person_a['country']}. followers number: {followers_a}"
+    )
+    print(
+        f"B: {person_b["name"]}, a {person_b['description']}, from {person_b['country']}. followers number: {followers_b}"
     )
     # compare between score
     user_answer = input("who has more followers? Type 'A' or 'B': \n")
     if user_answer == "A" and followers_a > followers_b:
-        user_score += 1
-        print(followers_a)
-        print(followers_b)
-        print(f"you right! current score: {user_score}")
-
+        pass
     elif user_answer == "B" and followers_b > followers_a:
-        user_score += 1
-        print(followers_a)
-        print(followers_b)
-        print(f"you right! current score: {user_score}")
+        pass
     else:
-        # end of the game
         end_game = True
         print(f"you lose! current score: {user_score}")
+        return 0
+    print(f"you right! current score: {user_score+1}")
+    return 1
 
 
 end_game = False
 # genarate a random number
 user_score = 0
 number_a = randint(0, len(data) - 1)
-compare_a = data[number_a]
-followers_a = compare_a["follower_count"]
-
-print(
-    f"Compare A: {compare_a["name"]}, a {compare_a['description']}, from {compare_a['country']}.followers number: {followers_a} \n"
-)
-
+person_a = data[number_a]
+followers_a = person_a["follower_count"]
 
 while not end_game:
     number_b = randint(0, len(data) - 1)
-    while number_b == number_a:
+    print(f"{data[number_b]["name"]=} {data[number_a]["name"]=}")
+    while data[number_b] == data[number_a]:
+        print(f"{number_b=} {number_a=}")
         number_b = randint(0, len(data) - 1)
-    compare_b = data[number_b]
-    followers_b = compare_b["follower_count"]
+    if user_score:
+        followers_a = followers_b
+        person_a = person_b
+    person_b = data[number_b]
+    followers_b = person_b["follower_count"]
 
-    compare_followers(followers_b, followers_a, compare_b)
-    print(
-        f"Compare A: {compare_b["name"]}, a {compare_b['description']}, from {compare_b['country']}.followers number: {followers_b} \n"
-    )
+    user_score += compare_followers(followers_b, followers_a, person_b)
